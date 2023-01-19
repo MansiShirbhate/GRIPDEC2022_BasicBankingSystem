b39a85023fa25d1dfc44fd7ab95191306dec5ab4
Mansi Shirbhate <121678285+MansiShirbhate@users.noreply.github.com>
2023-01-19 22:38:04 +0530

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,21 @@
+<link rel="stylesheet" href="navbar.css">
+<!-- navbar --> 
+<nav class="navbar navbar-expand-md navbar-light bg-light">
+      <a href="index.php"><img src="CB.png" alt="" width="80px" height="60px"></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="index.php">Home</a>
+              </li>
+             
+              <li class="nav-item">
+                <a class="nav-link" href="transfermoney.php">Make a Transaction</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transactionhistory.php">Transaction History</a>
+              </li>
+          </div>
+       </nav>
